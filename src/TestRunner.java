7780722d0a887989195a183f1fe03c3c62d26187
TimepieceMaster <jacvill@att.net>
2019-04-07 13:42:01 -0500

    Updated TestRunner with TestProblem5

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -8,7 +8,7 @@ public class TestRunner {
 	public static void main(String[] args)
 	{
 		Result result = JUnitCore.runClasses(TestProblem1.class,
-				TestProblem2.class, TestProblem3.class, TestProblem4.class, TestProblem6.class);
+				TestProblem2.class, TestProblem3.class, TestProblem4.class, TestProblem5.class, TestProblem6.class);
 		
 		for(Failure failure : result.getFailures())
 			System.out.println(failure.toString());
